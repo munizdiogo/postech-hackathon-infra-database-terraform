478a02e22c7ed13b48e3eb346b77452943bc8ba4
Diogo Muniz <diogomuniz.web@gmail.com>
2024-03-22 20:58:55 -0300

    Adicionado auto_increment no ID

--- scripts/init.sql
+++ scripts/init.sql
@@ -5,23 +5,23 @@ CREATE DATABASE IF NOT EXISTS dbpostech_colaboradores;
 USE dbpostech_colaboradores;
 
 
-CREATE TABLE IF NOT EXISTS `colaboradores` (
-    `id` BIGINT(20) NOT NULL,
-    `status` VARCHAR(20) NOT NULL,
-    `cadastrado_em` DATETIME NOT NULL,
-    `atualizado_em` DATETIME NOT NULL,
-    `nome` VARCHAR(60) NOT NULL,
-    `cpf` VARCHAR(14) NOT NULL,
-    `email` VARCHAR(200) NOT NULL,
-    `telefone` VARCHAR(20),
-    `data_nascimento` DATE NOT NULL,
-    `horario_inicio_expediente` DATETIME,
-    `horario_termino_expediente` DATETIME,
-    `horario_inicio_almoco` DATETIME,
-    `horario_termino_almoco` DATETIME,
-    PRIMARY KEY (`id`),
-    UNIQUE (`id`, `cpf`)
-);
+CREATE TABLE `colaboradores` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `status` varchar(20) NOT NULL,
+  `cadastrado_em` datetime NOT NULL,
+  `atualizado_em` datetime NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `data_nascimento` date NOT NULL,
+  `horario_inicio_expediente` datetime DEFAULT NULL,
+  `horario_termino_expediente` datetime DEFAULT NULL,
+  `horario_inicio_almoco` datetime DEFAULT NULL,
+  `horario_termino_almoco` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`,`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
@@ -44,4 +44,4 @@ CREATE TABLE IF NOT EXISTS `registros_de_ponto` (
     `data_hora_inicio` DATETIME NOT NULL,
     `data_hora_termino` DATETIME NOT NULL,
     PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
